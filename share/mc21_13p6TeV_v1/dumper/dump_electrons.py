0af6dab14be8514e3fd100e7d96a5ea4282f3490
lbarrade <lucas.nunes@cern.ch>
2023-05-09 17:25:17 -0300

    Adds triggering functionalities to dump_electrons.py

--- share/mc21_13p6TeV_v1/dumper/dump_electrons.py
+++ share/mc21_13p6TeV_v1/dumper/dump_electrons.py
@@ -5,12 +5,45 @@ from egamma import ToolSvc, GeV
 from egamma import ElectronLoop
 from egamma.enumerators import Dataframe as DataframeEnum
 from egamma.core import complete
+from egamma.core import list_files
 
 import argparse
 import sys,os
 import traceback
 import numpy as np
 np.random.seed(512)
+
+
+class MyFilter:
+    def __init__ ( self, jets=False):
+        self.jets=jets
+
+    def __call__(self, ctx):
+
+        elCont = ctx.getHandler( "ElectronContainer" )
+        if elCont.et() < 2*GeV:
+            return False
+
+        fc = ctx.getHandler( "HLT__TrigEMClusterContainer" )
+        if not fc.isGoodRinger():
+            return False
+
+        #
+        # Monte Carlo selection
+        #
+        mc = ctx.getHandler("MonteCarloContainer")
+        if self.jets:
+            if mc.isTruthElectronFromAny():
+                return False
+        else:
+            if fc.et() < 15*GeV: # Jpsiee
+                if not mc.isTruthElectronFromJpsiPrompt():
+                    return False
+            else: # Zee
+                if not mc.isTruthElectronFromZ():
+                    return False
+        return True
+
 
 parser = argparse.ArgumentParser(description = '', add_help = False)
 parser = argparse.ArgumentParser()
@@ -20,7 +53,7 @@ parser = argparse.ArgumentParser()
 #
 
 parser.add_argument('-i','--inputFile', action='store',
-    dest='inputFile', required = True,
+    dest='inputFile', required = True, nargs='+',
     help = "The input file.")
 
 parser.add_argument('-o','--outputFile', action='store',
@@ -43,28 +76,32 @@ parser.add_argument('--mute', action='store_true',
     dest='mute', required = False, 
     help = "Use this for production. quite output")
 
-
 parser.add_argument('-j','--job_id', action='store',
     dest='job_id', required = False, default=0, type=int,
     help = "The job id for parallel processing.")
+
+parser.add_argument('--ringerVersion', action='store',
+    dest='ringerVersion', required = False,
+    help = "The ringer version")
+
+parser.add_argument("--triggers", action="store",
+    nargs="+", required=False, default=[],
+    help="Triggers to be included")
+
+parser.add_argument("--jets", action="store_true",
+                    help="If passed considers the input data as jets")
+
+
 #
 # event selection configuration
 #
 
-
-
-if len(sys.argv)==1:
-  parser.print_help()
-  sys.exit(1)
-
 args = parser.parse_args()
-
+input_files = list_files(args.inputFile, "root")
 
 try:
-
-
     acc = ElectronLoop(  "EventATLASLoop",
-                         inputFile  = args.inputFile,
+                         inputFile  = input_files,
                          treePath   = args.path,
                          dataframe  = DataframeEnum.Run3,
                          outputFile = args.outputFile,
@@ -73,62 +110,26 @@ try:
                          abort      = False,
                       )
 
-
-    class MyFilter:
-        def __init__ ( self, jets=False):
-            self.jets=jets
-
-        def __call__(self, ctx):
-
-            elCont = ctx.getHandler( "ElectronContainer" )
-            if elCont.et() < 2*GeV:
-                return False
-
-            fc = ctx.getHandler( "HLT__TrigEMClusterContainer" )
-            if not fc.isGoodRinger():
-                return False
-
-            #
-            # Monte Carlo selection
-            #
-            mc = ctx.getHandler("MonteCarloContainer")
-            if self.jets:
-                if mc.isTruthElectronFromAny():
-                    return False
-            else:
-                if fc.et() < 15*GeV: # Jpsiee
-                    if not mc.isTruthElectronFromJpsiPrompt():
-                        return False
-                else: # Zee
-                    if not mc.isTruthElectronFromZ():
-                        return False
-            return True
-
-
-    jets = True if 'JF17' in args.inputFile else False
-    my_filter = MyFilter(jets)
-
+    my_filter = MyFilter(args.jets)
 
     #
     # Initial filter
     #
-
+    from egamma.emulator import electronFlags
+    electronFlags.ringerVersion = args.ringerVersion
+    
     from egamma import Filter
     ToolSvc+=Filter( "Filter", [my_filter])
-
 
     #
     # Electron dumper
     #
     from egamma.dumper import ElectronDumper_v2 as ElectronDumper
-    output = args.inputFile.split('/')[-1].replace('.root','')
+    dumper_output = args.outputFile.replace('.root', f'_{args.job_id}')
     et_bins = [3, 7, 10, 15, 20, 30, 40, 50, 1000000]
     eta_bins = [0.0, 0.8, 1.37, 1.54, 2.37, 2.50]
 
-
-    dumper = ElectronDumper(output, et_bins, eta_bins )
-
-
+    dumper = ElectronDumper(dumper_output, et_bins, eta_bins)
 
     def isZ_decorator( ctx ):
         mc = ctx.getHandler("MonteCarloContainer")
@@ -140,21 +141,20 @@ try:
         mc = ctx.getHandler("MonteCarloContainer")
         return mc.isTruthElectronFromJpsiPrompt()
     def target( ctx ):
-        # get the target value from the sample
-        return 0 if jets else 1 # signal (Zee or Jpsiee)
-
-
+        return 0 if args.jets else 1
+    
+    for trigName in args.triggers:
+        dumper.decorate_chain(trigName)
     dumper.decorate( "mc_isTruthElectronFromZ"          , isZ_decorator   )
     dumper.decorate( "mc_isTruthElectronFromAny"        , isAny_decorator )
     dumper.decorate( "mc_isTruthElectronFromJpsiPromt"  , isJpsi_decorator)
     dumper.decorate( "target", target)
 
-
     ToolSvc+=dumper
     acc.run(args.nov)
     print('job done')
     os.system('rm %s'%args.outputFile)
-    complete(args.job_id)
+    # complete(args.job_id)
     sys.exit(0)
 
 except  Exception as e:
