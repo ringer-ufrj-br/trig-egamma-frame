879bb297747ffa6c0b235627842d58b04c70f7c8
Lucas Barra de Aguiar Nunes <lucas.nunes@cern.ch>
2024-03-19 13:34:10 +0000

    Adds scripts/test_root_id_uniqueness.py

---
+++ scripts/test_root_id_uniqueness.py
@@ -0,0 +1,59 @@
+import os
+import numpy as np
+import logging
+import logging.config
+from typing import List
+from argparse import ArgumentParser
+from egamma.logging import set_loggers
+import ROOT
+from egamma.root import get_tchain
+
+LOGGER_NAME = 'trig-egamma-frame-debug'
+
+
+def parse_args():
+    parser = ArgumentParser(
+        prog=os.path.basename(__file__),
+        description="Tests if the id col in a collection of root "
+        "files is unique"
+    )
+    parser.add_argument(
+        '--filepaths',
+        dest='filepaths',
+        nargs='+',
+        help='Path to the files to be tested.',
+        required=True
+    )
+    parser.add_argument(
+        '--treepath',
+        required=True,
+        help='The path to the tree object inside the root file'
+    )
+
+    args = parser.parse_args().__dict__
+    return args
+
+
+def main(
+        filepaths: List[str],
+        treepath: str) -> None:
+
+    ROOT.EnableImplicitMT()
+
+    _, chain = get_tchain(filepaths, treepath)
+    rdf = ROOT.RDataFrame(chain)
+    id_col = rdf.AsNumpy(['id'])['id']
+    n_entries = rdf.Count().GetValue()
+    n_unique_ids = len(np.unique(id_col))
+    logger.info(f'Number of entries: {n_entries}')
+    logger.info(f'Number of unique ids: {n_unique_ids}')
+
+
+if __name__ == "__main__":
+    set_loggers()
+    args = parse_args()
+    logger = logging.getLogger(LOGGER_NAME)
+    logger.info(args)
+    logger.info('Computing unique ids in root files')
+    main(**args)
+    logger.info('Finished')
