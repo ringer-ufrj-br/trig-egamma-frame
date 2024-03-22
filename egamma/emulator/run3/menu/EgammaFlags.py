b773659eb778c0e0371c209702af36b9a5c42608
Lucas Barra de Aguiar Nunes <lucas.nunes@cern.ch>
2024-03-22 09:27:24 +0000

    Undoes wrong change in EgammaFlags.py

--- egamma/emulator/run3/menu/EgammaFlags.py
+++ egamma/emulator/run3/menu/EgammaFlags.py
@@ -11,7 +11,7 @@ class ElectronFlags:
         Attention to the paths used as default here. Any new versions must be correctly defined.
         When define a chain remenber to put the key version on it like: 'HLT_e26_lhtight_ivarloose_run2-v8_L1EM22VHI'
         '''
-        basepath = "/root/cluster_dir/data"
+        basepath = os.environ['CERN_DATA']
         self.ringerVersion = {
             'run2_v8' : os.path.join(basepath, "joao.pinto/tunings/releases/Run2_20230227_v8/"),
             'run3_v0' : os.path.join(basepath, "joao.pinto/tunings/releases/Run3_20230316_v0/"),
