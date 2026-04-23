42a20816c03427b383166e3905678e3c814638a2
jodafons <jodafons@lps.ufrj.br>
2026-04-23 13:54:44 -0300

    update

--- tests/data_collect.py
+++ tests/data_collect.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
 import sys
 import ROOT
-import trig_egamma_frame
+import numpy as np
+
+from trig_egamma_frame import ElectronLoop, DataframeSchemma, EventContext, GeV, ToolSvc
+from trig_egamma_frame.algorithms.dumper import ElectronDumper_v2 as ElectronDumper
+from trig_egamma_frame.algorithms import Filter, EventFilter, isAny_decorator, isZ_decorator, isJpsi_decorator
+from trig_egamma_frame.algorithms import Efficiency, Quadrant
 
 def main():
+
     print("🚀 Starting data collection test...")
+
+    input_files = "tests/samples/run_2/*.root"
+   
+    acc = ElectronLoop(  "EventATLASLoop",
+                         inputFile  = input_files,
+                         treePath   = "*/HLT/Physval/Egamma/probes",
+                         dataframe  = DataframeSchemma.Run2,
+                         outputFile = "output.root",
+                         abort = True,
+                      )
+
+    ToolSvc+=Filter( "Filter", [EventFilter(is_data=True, is_background=False)])
+    acc.run(10000)
     # Dummy logic for data collection
     print("✅ Data collection successful!")
     sys.exit(0)
